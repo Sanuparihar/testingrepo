80ee1712df8e7993d0274c65a123a88066cf7e92
Sanuparihar <pariharsanu87@gmail.com>
2023-11-09 12:58:37 +0000

    script

---
+++ functionbackup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function  create_backup {
+
+	src_dir=/home/ubuntu/scripts
+	tgt_dir=/home/ubuntu/backups
+
+	current_timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
+
+
+	final_file=$tgt_dir/scripts-backup-$current_timestamp.tgz
+
+	tar czf $final_file -C $src_dir .
+}
+echo "starting backup process"
+create_backup
+echo "backup completed"
